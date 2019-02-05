1d931ca93685d756ebdba2d8c59e32f93ce8d3a4
Sam Arrants <arrants8@gmail.com>
2019-02-04 23:41:49 -0600

    homework changes

--- student_code.py
+++ student_code.py
@@ -83,6 +83,10 @@ class KnowledgeBase(object):
                     self.rules[ind].asserted = True
 
     def kb_assert(self, fact_rule):
+        
+        
+
+        
         """Assert a fact or rule into the KB
 
         Args:
@@ -116,22 +120,119 @@ class KnowledgeBase(object):
             print("Invalid ask:", fact.statement)
             return []
 
-    def kb_retract(self, fact):
-        """Retract a fact from the KB
+   
+            """An asserted fact should only be removed if it is unsupported.
+An asserted rule should never be removed.
+Use the supports_rules and supports_facts fields to find and adjust
+facts and rules that are supported by a retracted fact.
+The supported_by lists in each fact/rule that it supports needs to
+be adjusted accordingly.
+If a supported fact/rule is no longer supported as a result of
+retracting this fact (and is not asserted), it should also be removed."""
 
-        Args:
-            fact (Fact) - Fact to be retracted
+    def kb_delete(self, fact_or_rule):
+        if isinstance(fact_or_rule, Fact):
+            fact = self._get_fact(fact_or_rule)
+            #if asserted or supported, return
+            if fact.asserted or fact.supported_by:
+                return
+            #if not asserted and empty, delete 
+            else:
+                #look at each supported fact, remove fact from supported things, see if fact needs to be deleted
+                for x in fact.supported_facts:
+                    newfact = self._get_fact(x)
+                    #remove from supported by 
+                    for y in newfact.supported_by:
+                        if newfact.supported_by[y][0].statement == fact.statement:
+                            newfact.supported_by.remove([y][0])       
+                    self.kb_delete(fact)
+                    return
+                for x in fact.supported_rules:
+                    newrule = self._get_rule(x)
+                    #remove from supported by 
+                    for y in newrule.supported_by:
+                        if newrule.supported_by[y][0] == rule:
+                            newrule.supported_by.remove([y][0])       
+                    self.kb_delete(rule)
+                
 
-        Returns:
-            None
-        """
-        printv("Retracting {!r}", 0, verbose, [fact])
+
+        #if passed a rule 
+        else:
+            rule = self._get_rule(fact_or_rule)
+            #if asserted or supported, return
+            if rule.asserted or rule.supported_by:
+                return
+            #if not asserted and empty, delete 
+            else:
+                #look at each supported fact, remove fact from supported things, see if fact needs to be deleted
+                for x in rule.supported_facts:
+                    newfact = self._get_fact(x)
+                    #remove from supported by 
+                    for y in newfact.supported_by:
+                        if newfact.supported_by[y][0].statement == fact.statement:
+                            newfact.supported_by.remove([y][0])       
+                    self.kb_delete(fact)
+                    return
+                for x in rule.supported_rules:
+                    newrule = self._get_rule(x)
+                    #remove from supported by 
+                    for y in newrule.supported_by:
+                        if newrule.supported_by[y][0] == rule:
+                            newrule.supported_by.remove([y][0])       
+                    self.kb_delete(rule)
+                    return
+         
+
+
+    
+
+    def kb_retract(self, fact1):
+         fact = self._get_rule(fact1)
+         if fact.asserted:
+             fact.asserted = False
+         self.kb_delete(fact)
+         return
+         
+         printv("Retracting {!r}", 0, verbose, [fact])
         ####################################################
         # Student code goes here
         
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
+
+        newBindings = match(fact.statement, rule.lhs[0])
+
+
+        ## if there is a match with the fact and 1st element of LHS,
+     
+
+        if newBindings:
+            ## if there is only one LHS statement, add RHS as new fact
+            if len(rule.lhs) == 1:
+                newFact = Fact(instantiate(rule.rhs, newBindings), [[fact, rule]])
+                rule.supports_facts.append(newFact)
+                fact.supports_facts.append(newFact)
+                newFact.asserted = False
+                kb.kb_assert(newFact)
+
+
+
+            ## if there are multiple statements on LHS, add new rule with bindings and without 1st el
+            else:
+                onLHS = []
+                for curr in rule.lhs[1:]:
+                    onLHS.append(instantiate (curr, newBindings))
+                    
+                newRule = Rule([onLHS, (instantiate(rule.rhs,newBindings))], [[fact, rule]])
+                rule.supports_rules.append(newRule)
+                fact.supports_rules.append(newRule)
+                newRule.asserted = False
+                kb.kb_assert(newRule)        
+
+
+        
         """Forward-chaining to infer new facts and rules
 
         Args:
